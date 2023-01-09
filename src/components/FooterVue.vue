b6d57570e21c5f6e457cbfe382cd304768a7b0dd
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-09 15:58:59 +0100

    footer structure

--- src/components/FooterVue.vue
+++ src/components/FooterVue.vue
@@ -5,9 +5,68 @@
 </script>
 
 <template>
-  <h3>footer</h3>
+  <footer class="bg-dark text-white">
+    <div class="container">
+
+      <section id="footer-link" class="d-flex flex-column flex-wrap">
+        <article>
+          <h2>Title</h2>
+          <ul>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+          </ul>
+        </article>
+  
+        <article>
+          <h2>Title</h2>
+          <ul>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+          </ul>
+        </article>
+  
+        <article>
+          <h2>Title</h2>
+          <ul>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+          </ul>
+        </article>
+  
+        <article>
+          <h2>Title</h2>
+          <ul>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+            <li class="list-group-item">link</li>
+          </ul>
+        </article>
+  
+      </section>
+  
+      <section id="footer-bottom" class="bg-dark d-flex">
+        <button>SIGN-UP NOW!</button>
+        <div class="ms-auto">social</div>
+      </section>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
+
+#footer-link{
+  height: 250px;
+}
   
 </style>
