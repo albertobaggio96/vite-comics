a8f3e43fe6d0dc007c44e3bbd13d5e234f5f5d59
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-10 14:52:53 +0100

    use a function to take img from js

--- src/components/main/DCMerchandising.vue
+++ src/components/main/DCMerchandising.vue
@@ -4,29 +4,34 @@
       return{
         merchandisingList:[
           {
-            icon: "../../public/img/buy-comics-digital-comics.png",
+            icon: "buy-comics-digital-comics.png",
             description: "Digital Comics"
           },
           {
-            icon: "../../public/img/buy-comics-merchandise.png",
+            icon: "buy-comics-merchandise.png",
             description: "DC Merchandise"
           },
           {
-            icon: "../../public/img/buy-comics-subscriptions.png",
+            icon: "buy-comics-subscriptions.png",
             description: "Subscriptions"
           },
           {
-            icon: "../../public/img/buy-comics-shop-locator.png",
+            icon: "buy-comics-shop-locator.png",
             description: "Comic Shop Locator"
           },
           {
-            icon: "../../public/img/buy-dc-power-visa.svg",
+            icon: "buy-dc-power-visa.svg",
             description: "DC Power Visa"
           },
           
         ]
       }
-    }
+    },
+    methods: {
+      getImagePath(imgPath){
+        return new URL(`../../assets/img/${imgPath}`, import.meta.url).href;
+      }
+    },
   }
 </script>
 
@@ -34,7 +39,7 @@
   <section id="icon-banner" class="dc-bg-color">
     <div class="container py-5 d-flex justify-content-around">
         <div v-for="merchandising in merchandisingList" class="d-flex align-items-center">
-          <img :src="`${merchandising.icon}`" :alt="merchandising.description" class="me-3">
+          <img :src="getImagePath(merchandising.icon)" :alt="merchandising.description" class="me-3">
           <div>{{ merchandising.description.toUpperCase() }}</div>
         </div>
     </div>
